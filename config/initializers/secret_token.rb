fb838546afe3b6d4268c99fb3aa2fad4a32836f6
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-05-06 15:30:56 -0700

    Start rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodfood::Application.config.secret_token = '872e55370f058c0e4cad7050498a5ced60f7700805408d7ed84b2881b801853351a946314bb921455e15604e07e29b1fcd93fd6cb87dc7d7a020fff1c434e39f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Goodfood::Application.config.session_store :cookie_store, key: '_goodfood_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Goodfood::Application.config.session_store :active_record_store
