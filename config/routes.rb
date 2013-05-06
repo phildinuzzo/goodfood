9183718e9f5d560b32e7d2729120906c087bc95e
phildinuzzo <phil248@gmail.com>
2013-05-06 16:17:39 -0700

    Added views again, edited route

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Goodfood::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'goodfood#index'
 
   # See how all your routes lay out with "rake routes"
 
