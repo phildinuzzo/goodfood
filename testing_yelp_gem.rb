4eed7011d9d53a7e1424e14ad3977516c71e5c80
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-05-09 15:11:59 -0700

    Delete commentted code.

--- testing_yelp_gem.rb
+++ testing_yelp_gem.rb
@@ -9,39 +9,6 @@ require 'openssl'
 require 'google_places'
 
 
-# require 'oauth'
-
-# consumer_key = 'fY6wOVe4OKHPb8FE_9Idmg'
-# consumer_secret = 'WXO7X8_eQvCEIYv28HPQhgkSZ2E'
-# token = 'slDSKW_7qjDqMl2YiCEGNAVD7Bn35fgS'
-# token_secret = 'eT9hoFWiTt52_HoYrQzi-jwT_ZY'
-# api_host = 'api.yelp.com'
-# consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-# access_token = OAuth::AccessToken.new(consumer, token, token_secret)
-# path = "/v2/search?term=restaurants&location=new%20york"
-# p access_token.get(path).body
-
-# def check_open(coordinates, name)
-#   if one_spot.nil? || one_spot['opening_hours'].nil?
-#     "Hours not given"
-#   else
-#     one_spot['opening_hours']['open_now']
-#   end
-# end
-
-# def grab_photo(coordinates, name)
-#   if one_spot.nil? || one_spot['photos'].nil?
-#     "no photo available"
-#   else
-#     photo_ref = one_spot['photos'][0]['photo_reference']
-#     "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{photo_ref}&sensor=true&key=AIzaSyDgjPw63xd4qJZ50P7a-vdvjx6FQHxX8CI"
-#   end
-# end
-
-# photo_url = grab_photo(coordinates, name)
-# google_rating = google_place_rating(coordinates, name)
-# place_open = check_open(coordinates, name)
-
 
 ENV['GOOGLE_PLACES'] = 'AIzaSyDgjPw63xd4qJZ50P7a-vdvjx6FQHxX8CI'
 ENV['YELP_WSID'] = 'tQd1EAsNyd6Pmdmt653HzA'
@@ -123,61 +90,3 @@ def good_food_places_info(address, city, state)
 end
 
 puts JSON.pretty_generate good_food_places_info("414 Brennan", "San Francisco", "CA")
-
-
-
-# Yelp search by phone number
-
-# request = Yelp::Phone::Request::Number.new(
-#             :phone_number => '(415) 345-8100 ',
-#             :yws_id => 'tQd1EAsNyd6Pmdmt653HzA')
-# response = client.search(request)
-# puts JSON.pretty_generate response
-
-
-# lat = 37.7939484886016
-# long = -122.402307987213
-# coordinates = "#{lat},#{long}"
-# name = "Wayfare Tavern"
-# name_array = name.split(" ")
-# spot = name_array[0]
-
-# check_open(coordinates, spot)
-
-
-
-
-# Get rating, phone, address from Google places
-
-# @client = GooglePlaces::Client.new('AIzaSyDfQUGytBit3OsfLDx8OygAllI9SJnXaIM')
-# spots = @client.spots(lat,long, :types => ['restaurant','food','bakery','cafe'])
-# refs = spots.map {|spot| spot.reference }
-# spots = refs.map {|ref| @client.spot(ref) }
-# places = spots.map do |spot|
-#    puts "#{spot.name} \n #{spot.rating} \n #{spot.formatted_phone_number} \n #{spot.formatted_address}"
-# end
-
-# spots = @client.spots(37.7939484886016, -122.402307987213, :types => ['restaurant','food','bakery','cafe'])
-# places = spots.map do |spot|
-#    puts "#{spot.name} \n #{spot.rating}"
-# end
-
-
-
-
-# Getting only open places at time of query
-
-# url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{long}&opennow&name=#{URI.escape(spot)}&radius=5000&types=food|restaurant|bakery|bar|cafe&sensor=false&key=AIzaSyDfQUGytBit3OsfLDx8OygAllI9SJnXaIM"
-# uri = URI.parse(URI.encode(url.strip))
-# file = open(uri, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)
-# places =  JSON.load(file.read)['results']
-# open_now = places.map {|place| puts "\n #{place['name']} \n #{place['rating']}\n\n #{place['reference']}" if place['rating'] >= 4}
-# # puts JSON.pretty_generate open_now
-
-
-
-# Getting photos from Google Places
-
-# photo_ref = CnRqAAAAYB95NEbhmbM-eixEeX9IajjkT6Tb4Xf8MeoDV1AzaVeWIVtWERpJa-JOmtPGz_jjsUIrrSKrBzx69pgGuiuKgJOgO9FeBMFcn29iy3E7aukRY3HjlW5HEyDa2qBVDrvqPnDXU0op7zM_12ZJsdNwABIQWwUjdwYCd68AcTQI6PKD8BoU0mFKaHK9MyX5OoF4xYd8je0eqv4
-# "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{photo_ref}&sensor=true&key=AIzaSyDfQUGytBit3OsfLDx8OygAllI9SJnXaIM"
-
