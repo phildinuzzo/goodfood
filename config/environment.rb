2ffe7013da9f9059a3e219eae1029392e2add8c5
Sherod Taylor <sherodtaylor@gmail.com>
2013-05-15 22:26:41 -0700

    removed env load hack

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-YAML.load_file("#{::Rails.root}/config/api_keys.yml")[::Rails.env].each {|k,v| ENV[k]=v}
 # Initialize the rails application
 Goodfood::Application.initialize!
