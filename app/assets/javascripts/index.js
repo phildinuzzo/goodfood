37796205c78843b3dd773bf8246bca348083dde0
andris310 <andris310@gmail.com>
2013-05-25 16:22:49 -0700

    Removed javascript file from search.html

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -46,4 +46,37 @@ $(document).ready(function(){
     console.log("no geolocation");
   }
 
+  function initialize() {
+
+      var input = /** @type {HTMLInputElement} */(document.getElementById('searchTextField'));
+      var autocomplete = new google.maps.places.Autocomplete(input);
+
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        input.className = '';
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+          // Inform the user that the place was not found and return.
+          input.className = 'notfound';
+          return;
+        }
+
+           var address = '';
+           if (place.address_components) {
+             address = [
+               (place.address_components[0] && place.address_components[0].short_name || ''),
+               (place.address_components[1] && place.address_components[1].short_name || ''),
+               (place.address_components[2] && place.address_components[2].short_name || '')
+             ].join(' ');
+           }
+
+           infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+           infowindow.open(map, marker);
+         });
+
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 }); // End of document ready
